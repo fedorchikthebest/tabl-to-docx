82ce15fdafddb8d259fbf7f98c6d26bff9ab1af7
Fedor Gorlenko <fedorgorlenko@gmail.com>
2024-11-26 18:00:10 +0300

    added reshenie_shablon and fixed bugs

--- test_generator.py
+++ test_generator.py
@@ -19,10 +19,11 @@ def render_shablons(csv_path, save_path):
     sheet_obj = wb_obj.active
     
     doc = DocxTemplate("shablon.docx")
+    doc_reshenie = DocxTemplate("attest_shablon.docx")
     row_num = 2
     
     while sheet_obj.cell(row = row_num, column = 1).value is not None:
-        contexts = [sheet_obj.cell(row_num, i).value for i in range(1, 11)]
+        contexts = [sheet_obj.cell(row_num, i).value for i in range(1, 12)]
         context = {'name' : contexts[0],
                 'born_date' : contexts[1],
                 'attes_num' : contexts[2],
@@ -32,10 +33,15 @@ def render_shablons(csv_path, save_path):
                 'num' : contexts[6],
                 'email': contexts[7],
                 'company': contexts[8],
-                'podpis': contexts[9]}
+                'podpis': contexts[9],
+                'reshenie_date': contexts[10]}
 
         doc.render(context)
-        doc.save(f"{save_path}\\{row_num}.docx")
+        doc.save(f"{save_path}\\{context['name']}_{context['num']}_{row_num}.docx")
+        
+        doc_reshenie.render(context)
+        doc_reshenie.save(f"{save_path}\\reshenie_{context['name']}_{context['num']}_{row_num}.docx")
+        
         row_num += 1
 
 
