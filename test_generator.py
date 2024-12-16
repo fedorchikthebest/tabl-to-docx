127dc87e8ca6d7b5b578517c20d7a33a40d7cc43
gimonchik <73658246+fedorchikthebest@users.noreply.github.com>
2024-12-16 08:51:58 +0300

    Update test_generator.py

--- test_generator.py
+++ test_generator.py
@@ -28,11 +28,11 @@ def render_shablons(csv_path, save_path):
     wb_obj = load_workbook(csv_path)
     sheet_obj = wb_obj.active
     
-    doc = DocxTemplate("shablon.docx")
-    doc_reshenie = DocxTemplate("attest_shablon.docx")
     row_num = 2
     
     while sheet_obj.cell(row = row_num, column = 1).value is not None:
+        doc = DocxTemplate("shablon.docx")
+        doc_reshenie = DocxTemplate("attest_shablon.docx")
         contexts = [sheet_obj.cell(row_num, i).value for i in range(1,10)]
         context = {'name' : to_kavichki(contexts[0]),
                 'born_date' : contexts[1],
